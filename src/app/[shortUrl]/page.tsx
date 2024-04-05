827bd6b4cb40527471c577aa906e5ea65d076a24
joscacgom <jose9_negro@hotmail.com>
2024-04-05 16:59:39 +0200

    chore: redirection almost working

--- src/app/[shortUrl]/page.tsx
+++ src/app/[shortUrl]/page.tsx
@@ -1,53 +1,19 @@
-"use client";
 import axios from "axios";
-import { redirect, useParams } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
-import navigate from "@/actions/redirectTo";
+import { permanentRedirect } from "next/navigation";
 
-export default function RedirectionToOriginalUrl() {
-    const { shortUrl } = useParams();
-    const [originalUrl, setOriginalUrl] = useState("");
-    const [countdown, setCountdown] = useState(3);
-    const timerRef = useRef<any>(null);
+export default async function RedirectionToOriginalUrl({ params }: { params: { shortUrl: string } }) {
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(`https://url-shortener-func.azurewebsites.net/api/urlsByShortUrl/${shortUrl}`);
-                const data = response.data;
-                setOriginalUrl(data);
-
-                if (!timerRef.current) {
-                    const timerId = setInterval(() => {
-                        setCountdown((prevCount) => prevCount - 1); 
-                    }, 1000); 
-
-                    timerRef.current = timerId;
-
-                    setTimeout(async () => {
-                        clearInterval(timerId);
-                    }, 3000);
-                }
-
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
-
-        if (shortUrl) {
-            fetchData();
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(`https://url-shortener-func.azurewebsites.net/api/urlsByShortUrl/0i3BHO`);
+            const data = response.data;
+            return data;
+        } catch (error) {
+            // Handle errors
+            console.error("Error fetching data:", error);
         }
-        
-        return () => {
-            if (timerRef.current) {
-                clearInterval(timerRef.current);
-            }
-        };
-    }, [shortUrl]);
+    };
 
-    return (
-        <>
-            Redirecting to {originalUrl} in {countdown} seconds...
-        </>
-    );
+    const redirectUrl = await fetchData();
+    return permanentRedirect("https://"+redirectUrl);
 }
